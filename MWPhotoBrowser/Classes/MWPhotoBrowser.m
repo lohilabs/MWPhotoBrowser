42a0b1d84fc32580f6bbf44652231480bc99b605
Brant Young <brant.young@gmail.com>
2013-04-02 10:47:02 +0800

    Set tap to close popup instead of dismiss source view controller

--- MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -969,7 +969,11 @@ navigationBarBackgroundImageLandscapePhone = _navigationBarBackgroundImageLandsc
 
 - (BOOL)areControlsHidden { return (_toolbar.alpha == 0); /* [UIApplication sharedApplication].isStatusBarHidden; */ }
 - (void)hideControls { [self setControlsHidden:YES animated:YES permanent:NO]; }
-- (void)toggleControls { [self setControlsHidden:![self areControlsHidden] animated:YES permanent:NO]; }
+- (void)toggleControls {
+//    [self setControlsHidden:![self areControlsHidden] animated:YES permanent:NO];
+    [self dismissModalViewControllerAnimated:YES];
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationSlide];
+}
 
 #pragma mark - Properties
 
